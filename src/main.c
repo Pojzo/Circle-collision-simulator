358378a4aa1d13fbe59d483aed7cae02517ad747
Pojzo <pojzinko8@gmail.com>
2021-11-19 16:31:14 +0100

    Test

--- src/main.c
+++ src/main.c
@@ -1,7 +1,4 @@
-// SDL2 Hello, World!
-// This should display a white screen for 2 seconds
-// compile with: clang++ main.cpp -o hello_sdl2 -lSDL2
-// run with: ./hello_sdl2
+// // test
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include "rect.h"
