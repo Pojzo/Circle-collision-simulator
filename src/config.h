1a33ba7631ebe5cc032254444cba643a815c59da
Pojzo <pojzinko8@gmail.com>
2021-11-20 15:24:00 +0100

    Removed unnecessary include

--- src/config.h
+++ src/config.h
@@ -1,6 +1,5 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-#include "color.h"
 
 static const int SCREEN_WIDTH = 640;
 static const int SCREEN_HEIGHT = 480;
@@ -12,6 +11,5 @@ static const int BACKGROUND_R = 255;
 static const int BACKGROUND_G = 255;
 static const int BACKGROUND_B = 255;
 static const int BACKGROUND_A = 255;
-
 
 #endif
