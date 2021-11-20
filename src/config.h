58da4657a8ca45de48a0d695edb46cb4be7feed5
Pojzo <pojzinko8@gmail.com>
2021-11-20 15:17:14 +0100

    Added background color

--- src/config.h
+++ src/config.h
@@ -1,9 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#include "color.h"
 
 static const int SCREEN_WIDTH = 640;
 static const int SCREEN_HEIGHT = 480;
 static const int FPS = 30;
 static const float DELAY_MS = 1000. / FPS;
+static const char WINDOW_NAME[]= "Window";
+
+static const int BACKGROUND_R = 255;
+static const int BACKGROUND_G = 255;
+static const int BACKGROUND_B = 255;
+static const int BACKGROUND_A = 255;
+
 
 #endif
