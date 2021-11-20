b091be1d070287751e94feb6af2afa588e71fdd2
Pojzo <pojzinko8@gmail.com>
2021-11-20 15:09:11 +0100

    Changed constructor for color

--- src/color.c
+++ src/color.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "color.h"
 
 #define COLOR_MAX 255
@@ -10,7 +11,8 @@ color_t COLOR_GREEN = {.r = 0, .g = 255, .b = 0, .a = 255};
 color_t COLOR_BLUE = {.r = 0, .g = 0, .b = 255, .a = 255};
 
 // constructor for struct Color
-int color_ctor (color_t *c, int r_, int g_, int b_, int a_) {
+color_t *color_ctor(int r_, int g_, int b_, int a_) {
+    color_t *color = (color_t *)malloc(sizeof(color_t));
     // variables have to be in range COLOR_MIN - COLOR_MAX
     if (r_ < COLOR_MIN || r_ > COLOR_MAX) {
         fprintf (stderr, "RGB value must be in range 0 - 255");
@@ -32,12 +34,12 @@ int color_ctor (color_t *c, int r_, int g_, int b_, int a_) {
         return 0; 
     }
     // assign rgba valuesj
-    c->r = r_;
-    c->g = g_;
-    c->b = b_;
-    c->a = a_;
+    color->r = r_;
+    color->g = g_;
+    color->b = b_;
+    color->a = a_;
 
-    return 1;
+    return color;
 }
 
 // export color constants
--- src/color.h
+++ src/color.h
@@ -8,6 +8,6 @@ typedef struct Color {
     int a; // alpha
 } color_t;
 
-int color_ctor (color_t *c, int r_, int g_, int b_, int a_);
+color_t *color_ctor(int r_, int g_, int b_, int a_);
 
 #endif
