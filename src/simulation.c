270a9210b999dbe3c40a925768692e675fd29ea6
Pojzo <pojzinko8@gmail.com>
2021-11-21 12:27:25 +0100

    Added function to detect collisions

--- src/simulation.c
+++ src/simulation.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "circle.h"
+#include <math.h>
 #include "simulation.h"
 #include "config.h"
 #include "time.h"
 
+#define abs(a) a > 0 ? a : -a
 
 int main(int argc, char **argv) {
     srand(time(NULL));
@@ -29,37 +30,26 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    const int num_circles = 5;
+    const int num_circles = 10;
+    const int radius = 30;
     circle_t *circles[num_circles];  // test circle
     for (int i = 0; i < num_circles; i++) {
         circle_t *circle;
-        
-        circle = circle_ctor((float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2, 50, rand() % 255, rand() % 255, rand() % 255, 255); // constructor for circle
-        //circle_set_vel_x(circle, rand() % 5 * -1 * rand() % 2); // set x velocity of circle to 5
-        //circle_set_vel_x(circle, rand() % 5 * -1 * rand() % 2); // set x velocity of circle to 5
+        circle = circle_ctor((i + 1) * radius * 2 + 20, i * radius * 2 + 20, radius, rand() % 255, rand() % 255, rand() % 255, 255); // constructor for circle
         circle_set_vel_x(circle, rand() % 10); // set x velocity of circle to 5
         circle_set_vel_y(circle, rand() % 10); // set y velocity of circle to 5
         circles[i] = circle;
     }
-    /*
-    circle_t *circle1 = circle_ctor((float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2, 50, 0, 255, 0, 255); // constructor for circle
-    circle_set_vel_x(circle1, rand() % 10); // set x velocity of circle to 5
-    circle_set_vel_y(circle1, rand() % 10); // set y velocity of circle to 5
-
-    circle_t *circle2 = circle_ctor((float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2, 50, 0, 255, 0, 255); // constructor for circle
-    circle_set_vel_x(circle2, rand() % 10); // set x velocity of circle to 5
-    circle_set_vel_y(circle2, rand() % 10); // set y velocity of circle to 5
-    */
-
-
+    
     for (;;) {
         refresh_screen(renderer);
         for (int i = 0; i < num_circles; i++) {
+            detect_collision(circles, num_circles);
             circle_move(circles[i]); // move circle with its velocity
             circle_draw(renderer, circles[i]); // draw circle to screen
         }
         SDL_RenderPresent(renderer); //
-        //SDL_Delay(DELAY_MS); // add delay between frames
+        SDL_Delay(DELAY_MS); // add delay between frames
 
         if (handle_quit()) { // if user pressed close button, handle quit returns 1
             printf ("Terminating program\n");
@@ -86,7 +76,7 @@ SDL_Window *init_window() {
 // constructor for SDL_Renderer
 SDL_Renderer *init_renderer(SDL_Window *window) {
     // create an instance of renderer
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);//SDL_RENDERER_ACCELERATED 
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
 
     return renderer;
@@ -112,3 +102,35 @@ int handle_quit() {
     }
     return 0;
 }
+
+// detect collision between circles
+void detect_collision(circle_t **circles, int num_circles) {
+    float diff_x; // x difference between centres of circles
+    float diff_y; // y difference between centres of circles
+    float distance; // euclidian distance between centres of circles
+    float minimum_distance;
+
+    for (int i = 0; i < num_circles; i++) {
+        for (int j = 0; j < num_circles; j++) {
+            if (i == j) {
+                continue;
+            }
+            diff_x = abs(circles[i]->pos_x - circles[j]->pos_x);
+            diff_y = abs(circles[i]->pos_y - circles[j]->pos_y);
+            distance = diff_x * diff_x + diff_y * diff_y; // use pythagoeran therom to calculate distance
+            minimum_distance = (circles[i]->radius + circles[j]->radius) * (circles[i]->radius + circles[j]->radius);
+
+            if (distance < minimum_distance) { // if the distance is smalle
+                // reverese velocities of both circles
+                collide(circles[i], circles[j]);
+            }
+        }
+    }
+}
+
+/*
+// function adds a circle to simulation, without ovelapping with already added ones
+int add_circle(circle_t **circles) {
+return 1;
+}
+*/
--- src/simulation.h
+++ src/simulation.h
@@ -1,10 +1,12 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 #include <SDL2/SDL.h>
+#include "circle.h"
 
 SDL_Window *init_window();
 SDL_Renderer *init_renderer(SDL_Window *window);
 void refresh_screen(SDL_Renderer *renderer);
 int handle_quit();
+void detect_collision(circle_t **circles, int num_circles);
 
 #endif
