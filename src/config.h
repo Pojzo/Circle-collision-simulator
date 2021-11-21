90236408cfa89025436f7fd0386c18688b458a5e
Pojzo <pojzinko8@gmail.com>
2021-11-21 12:27:47 +0100

    Added default mass constant

--- src/config.h
+++ src/config.h
@@ -7,6 +7,8 @@ static const int FPS = 60;
 static const float DELAY_MS = 1000. / FPS;
 static const char WINDOW_NAME[]= "Window";
 
+static const int DEFAULT_CIRCLE_MASS = 1;
+
 static const int BACKGROUND_R = 255;
 static const int BACKGROUND_G = 255;
 static const int BACKGROUND_B = 255;
