1f69fae3e4cb9c83b0b83b4d0719bbf47054def3
Pojzo <pojzinko8@gmail.com>
2021-11-23 15:51:48 +0100

    New function vector_copy

--- src/vector.h
+++ src/vector.h
@@ -10,7 +10,9 @@ vector_t *vector_create(float x_, float y_);
 float vector_dot_product(vector_t *first, vector_t *second);
 vector_t *vector_multiply(vector_t* v, float x);
 vector_t *vector_subtract(vector_t *first, vector_t *second);
+vector_t *vector_copy(vector_t *v);
 void vector_free(vector_t *v);
 void vector_print(vector_t *v);
+
 
 #endif
